6cce352d13931ce12acc536c9afeca84b716d350
Greta <greta.johannsen@gmail.com>
2025-03-16 12:53:46 +0100

    Primer commit - subiendo proyecto

---
+++ src/main/java/es/greta/calendmeal/calendmeal/CalendMealApplication.java
@@ -0,0 +1,13 @@
+package es.greta.calendmeal.calendmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/greta/calendmeal/calendmeal/CalendMealApplicationTests.java
@@ -0,0 +1,13 @@
+package es.greta.calendmeal.calendmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
