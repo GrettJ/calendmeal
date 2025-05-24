6c75793c7f13eb068626d0d5d2bfee67e01c5f38
GrettJ <greta.johannsen@gmail.com>
2025-05-24 19:13:46 +0200

    se añaden ingredientes y alergenos

--- Diagrama y scripts/Calendmeal.sql
+++ Diagrama y scripts/Calendmeal.sql
@@ -41,3 +41,52 @@ CREATE TABLE ingredientes_alergenos (
 INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Marisco');
 INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Huevo');
 INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Mostaza');
+INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Apio');
+INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Sesamo');
+INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Pescado');
+INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Gluten');
+INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Lactosa');
+INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Frutos Secos');
+INSERT INTO alergenos (id, nombre) VALUES (nextval('alergenos_id_seq'), 'Cacahuete');
+
+
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Harina');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Huevo');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Mantequilla');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Aceite de Oliva');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Levadura');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Zanahoria');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Lechuga');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Tomate');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Calabacin');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Calabaza');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Mostaza');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Queso Parmesano');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Queso Mozarella');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Queso Gorgonzola');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Azucar');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Sal');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Agua');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Patata');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Leche');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Miel');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Ajo');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Lima');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Limon');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Crema de cacahuete');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Puerro');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Champiñones');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Hojaldre');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Nata para cocinar');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Azucar morena');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Yogur');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Brocoli');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Pasta');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Arroz');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Curry');
+INSERT INTO ingredientes (id, nombre) VALUES (nextval('ingredientes_id_seq'), 'Tomate frito');
+
+
+
+
+
